b6b50605821a6380a6d7d38197cbf7b871765690
Thomas Mårtensson <thomas@mamilldo.se>
2016-01-08 16:00:36 +0100

    Add: Responsiv oembed (markup för YT iframe som filtreras i content)

--- public/class-iis-pack-public.php
+++ public/class-iis-pack-public.php
@@ -81,6 +81,7 @@ class Iis_Pack_Public {
 	 */
 	public function filter_the_content() {
 		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/partials/add-license-to-images.php';
+		require_once plugin_dir_path( dirname( __FILE__ ) ) . 'public/partials/responsive-oembed.php';
 	}
 
 	/**
@@ -91,7 +92,7 @@ class Iis_Pack_Public {
 	public function iis_pack_enqueue_scripts() {
 
 		// Sammanslagen js för samtliga plugin
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/iis-pack-public.c160b1da.min.js', array( 'jquery' ), null, true );
+		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/iis-pack-public.35c63430.min.js', array( 'jquery' ), null, true );
 
 	}
 
---
+++ public/partials/responsive-oembed.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Filtrerar oembed (YT just nu)
+ *
+ * @link       https://www.iis.se
+ * @since      1.0.1
+ *
+ * @package    Iis_Pack
+ * @subpackage Iis_Pack/public/partials
+ */
+
+defined( 'WPINC' ) or die;
+
+/**
+ * Lägg iframe:en från standard-embedfunktionen i en div för att kunna göra den responsiv med CSS.
+ * @param string $code CSS-klass för Youtube-embeds.
+ */
+function iis_pack_custom_youtube_oembed( $code ) {
+	if ( stripos( $code, 'youtube.com' ) !== false && stripos( $code, 'iframe' ) !== false ) {
+		$code = str_replace( '<iframe', '<div class="embed-container"><iframe ', $code );
+		$code = str_replace( '</iframe>', '</iframe></div>', $code );
+		return $code;
+	}
+}
+add_filter( 'embed_oembed_html', 'iis_pack_custom_youtube_oembed' );
