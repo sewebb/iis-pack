6d2436de0042350492345def8a7a1a368097b9a3
Robert Hedman <robert.hedman@internetstiftelsen.se>
2019-04-09 16:19:10 +0200

    added anonymize IP to GA

--- public/partials/google-analytics.php
+++ public/partials/google-analytics.php
@@ -32,6 +32,7 @@ function add_iis_google_analytics() {
 
 		$printscript .= "<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');";
 		$printscript .= "ga('create', '" . $iis_ga_option_trackingid . "', 'auto');";
+		$printscript .= "ga('set', 'anonymizeIp', true);";
 		$printscript .= "ga('send', 'pageview');";
 
 
@@ -41,5 +42,3 @@ function add_iis_google_analytics() {
 		echo $printscript;
 	}
 }
-
-
