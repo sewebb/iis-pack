0ea45c98265d1520f1b82a063937d20dd24165c6
Richard Brattlund <info@richardbrattlund.se>
2022-03-29 16:06:36 +0200

    Bump

--- includes/class-iis-pack.php
+++ includes/class-iis-pack.php
@@ -69,7 +69,7 @@ class Iis_Pack {
 	public function __construct() {
 
 		$this->plugin_name = 'iis-pack';
-		$this->version = '2.0.0';
+		$this->version = '2.1.5';
 
 		$this->load_dependencies();
 		$this->set_locale();
