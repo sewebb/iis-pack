031170845089c7e72264c3e9713042f8e4a0ab5d
Jonas Nordström <jonas.nordstrom@gmail.com>
2022-10-04 15:26:03 +0200

    Bump version

--- includes/class-iis-pack.php
+++ includes/class-iis-pack.php
@@ -69,7 +69,7 @@ class Iis_Pack {
 	public function __construct() {
 
 		$this->plugin_name = 'iis-pack';
-		$this->version = '2.3.1';
+		$this->version = '2.3.2';
 
 		$this->load_dependencies();
 		$this->set_locale();
