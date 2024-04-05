f5cfa52614dcf3c8525bed9642ea4e5445745279
Richard Brattlund <info@richardbrattlund.se>
2024-04-05 15:12:48 +0200

    Bump version

--- iis-pack.php
+++ iis-pack.php
@@ -15,7 +15,7 @@
  * Plugin Name:       IIS Pack
  * Plugin URI:        https://internetstiftelsen.se
  * Description:       Functions for GTM, local avatars, removal of emoji scripts etc
- * Version:           3.0.0
+ * Version:           3.0.1
  * Author:            The Swedish Internet Foundation
  * Author URI:        https://internetstiftelsen.se
  * License:           GPL-2.0+
--- includes/class-iis-pack.php
+++ includes/class-iis-pack.php
@@ -69,7 +69,7 @@ class Iis_Pack {
 	public function __construct() {
 
 		$this->plugin_name = 'iis-pack';
-		$this->version     = '2.4.7';
+		$this->version     = '3.0.1';
 
 		$this->load_dependencies();
 		$this->set_locale();
